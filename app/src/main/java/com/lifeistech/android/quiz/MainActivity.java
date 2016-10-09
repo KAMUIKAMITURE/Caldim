90bc0aa38c0b93d8ec93aacae8684ceaec33819c
KAMUIKAMITURE <tab.git.kamui@gmail.com>
2016-10-09 17:31:30 +0900

    New sisstem

--- app/src/main/java/com/lifeistech/android/quiz/MainActivity.java
+++ app/src/main/java/com/lifeistech/android/quiz/MainActivity.java
@@ -8,40 +8,43 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends Activity implements Runnable, SensorEventListener {
 
-    private TextView answer1;
-    private TextView answer2;
-    private TextView answer3;
-    private TextView answer4;
-    private TextView count1;
-    private TextView count2;
-    private TextView sign;
-    Random r = new Random();
-    Random ra = new Random();
-    Random ran = new Random();
-    Random rando = new Random();
+    private static final int DIRECTION_UP = 0;
+    private static final int DIRECTION_DOWN = 1;
+    private static final int DIRECTION_RIGHT = 2;
+    private static final int DIRECTION_LEFT = 3;
+    private static final int DIRECTION_NONE = 4;
+
+    private TextView answerText_UP;
+    private TextView answerText_LEFT;
+    private TextView answerText_RIGHT;
+    private TextView answerText_DOWN;
+    private TextView questionText1;
+    private TextView questionText2;
+    private TextView Sign;
+    Random randomQuestion = new Random();
+
     SensorManager sm;
     float gx, gy, gz;
     Handler h;
-    int number;
-    int number2;
-    int number3;
-    int number4;
-    int number5;
-
+    int questionNumber1;
+    int questionNumber2;
+    int correctAnswer;
+    int scoreNumber;
+    int recordNumber;
+    float x = 0;
+    float y = 0;
+    float z = 0;
+    int answerDirection;
+    int number6;
 
 
     @Override
@@ -49,19 +52,16 @@ public class MainActivity extends Activity implements Runnable, SensorEventListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        answer1 = (TextView) findViewById(R.id.anser1);
-        answer2 = (TextView) findViewById(R.id.answer2);
-        answer3 = (TextView) findViewById(R.id.answer3);
-        answer4 = (TextView) findViewById(R.id.answer4);
-        count1 = (TextView) findViewById(R.id.count1);
-        count2 = (TextView) findViewById(R.id.count2);
-        sign = (TextView) findViewById(R.id.sign);
+        answerText_UP = (TextView) findViewById(R.id.anser1);
+        answerText_LEFT = (TextView) findViewById(R.id.answer2);
+        answerText_RIGHT = (TextView) findViewById(R.id.answer3);
+        answerText_DOWN = (TextView) findViewById(R.id.answer4);
+        questionText1 = (TextView) findViewById(R.id.count1);
+        questionText2 = (TextView) findViewById(R.id.count2);
+        Sign = (TextView) findViewById(R.id.sign);
         h = new Handler();
-        h.postDelayed((Runnable) this, 500);
+        h.postDelayed( this, 500);
         SharedPreferences data = getSharedPreferences("DataSave", Context.MODE_PRIVATE);
-
-
-
 
 
         sm = (SensorManager) getSystemService(SENSOR_SERVICE);
@@ -78,81 +78,130 @@ public class MainActivity extends Activity implements Runnable, SensorEventListe
     }
 
 
+    private void make() {
+        int firstQuestion = randomQuestion.nextInt(9);
+        int secondQuestion = randomQuestion.nextInt(9);
+        int sign = randomQuestion.nextInt(3);
+        answerDirection = randomQuestion.nextInt(4);
 
-    private void make(){
-        int ra = r.nextInt(9);
-        int ran = r.nextInt(9);
-        int rand = r.nextInt(3);
-        int answer = rando.nextInt(4);
+        questionNumber1 = firstQuestion;
+        questionText1.setText(String.valueOf(questionNumber1));
 
-            number = ra;
-            count1.setText(String.valueOf(number));
-
-            number2 = ran;
-            count2.setText(String.valueOf(number2));
-
+        questionNumber2 = secondQuestion;
+        questionText2.setText(String.valueOf(questionNumber2));
 
 
-        if (rand == 0) {
-            sign.setText("+");
+        if (sign == 0) {
+            this.Sign.setText("+");
             plus();
-        } else if (rand == 1) {
-            sign.setText("-");
+        } else if (sign == 1) {
+            this.Sign.setText("-");
             minus();
-        } else if (rand == 2) {
-            sign.setText("*");
+        } else if (sign == 2) {
+            this.Sign.setText("*");
             kakeru();
         }
-        if (answer == 0){
-            answer1.setText(String.valueOf(number3));
-            answer2.setText(String.valueOf(number3-1));
-            answer3.setText(String.valueOf(number3+1));
-            answer4.setText(String.valueOf(number3*2));
+        if (answerDirection == DIRECTION_UP) {
+            answerText_UP.setText(String.valueOf(correctAnswer));
+            answerText_LEFT.setText(String.valueOf(correctAnswer - 1));
+            answerText_RIGHT.setText(String.valueOf(correctAnswer + 1));
+            answerText_DOWN.setText(String.valueOf(correctAnswer + 10));
 
-        } else if (answer == 1){
-            answer1.setText(String.valueOf(number3+1));
-            answer2.setText(String.valueOf(number3));
-            answer3.setText(String.valueOf(number3*2));
-            answer4.setText(String.valueOf(number3-1));
-        } else if (answer == 2) {
-            answer1.setText(String.valueOf(number3-1));
-            answer2.setText(String.valueOf(number3*2));
-            answer3.setText(String.valueOf(number3));
-            answer4.setText(String.valueOf(number3+1));
-        }else {
-            answer1.setText(String.valueOf(number3*2));
-            answer2.setText(String.valueOf(number3+1));
-            answer3.setText(String.valueOf(number3-1));
-            answer4.setText(String.valueOf(number3));
+        } else if (answerDirection == 1) {
+            answerText_UP.setText(String.valueOf(correctAnswer + 1));
+            answerText_LEFT.setText(String.valueOf(correctAnswer));
+            answerText_RIGHT.setText(String.valueOf(correctAnswer + 10));
+            answerText_DOWN.setText(String.valueOf(correctAnswer - 1));
+        } else if (answerDirection == 2) {
+            answerText_UP.setText(String.valueOf(correctAnswer - 1));
+            answerText_LEFT.setText(String.valueOf(correctAnswer + 10));
+            answerText_RIGHT.setText(String.valueOf(correctAnswer));
+            answerText_DOWN.setText(String.valueOf(correctAnswer + 1));
+        } else {
+            answerText_UP.setText(String.valueOf(correctAnswer + 10));
+            answerText_LEFT.setText(String.valueOf(correctAnswer + 1));
+            answerText_RIGHT.setText(String.valueOf(correctAnswer - 1));
+            answerText_DOWN.setText(String.valueOf(correctAnswer));
         }
 
 //        doingAnswer();
 
     }
+
     private void plus() {
-        number3 = number + number2;
+        correctAnswer = questionNumber1 + questionNumber2;
 
     }
 
 
-
     private void minus() {
-        number3 = number - number2;
+        correctAnswer = questionNumber1 - questionNumber2;
 
 
     }
 
     private void kakeru() {
-        number3 = number * number2;
+        correctAnswer = questionNumber1 * questionNumber2;
 
     }
 
 
 
+    private int checkDerection() {
+        recordNumber++;
+        x = x + gx;
+        y = y + gy;
+        z = z + gz;
+//        Log.d("tags",String.valueOf(x));
+//        Log.d("tags",String.valueOf(y));
 
 
 
+        if (recordNumber == 5) {
+            x = 0;
+            y = 0;
+            z = 0;
+            recordNumber = 0;
+            if (y / 5 < -5) {
+                return DIRECTION_UP;
+//                number6 = 1;
+//                checkAnswer();
+            } else if (y / 5 > 5) {
+                return DIRECTION_DOWN;
+//                number6 = 2;
+//                checkAnswer();
+            } else if (x / 5 > 5) {
+                return DIRECTION_LEFT;
+//                number6 = 3;
+//                checkAnswer();
+            } else if (x / 5 < -5) {
+                return DIRECTION_RIGHT;
+//                number6 = 4;
+//                checkAnswer();
+            }
+        }
+        return DIRECTION_NONE;
+//                number6 = 0;
 
+
+    }
+
+
+    private void checkAnswer() {
+        int direction = checkDerection();
+        Log.d("DIRECTION", String.valueOf(direction));
+
+        if (answerDirection == direction) {
+//            正解のとき
+            make();
+            hozon();
+
+        } else if (direction != DIRECTION_NONE) {
+//          不正解のとき
+        } else {
+//          加速度センサ測定中
+        }
+    }
 
 
 //    private void doingAnswer(){
@@ -183,83 +232,56 @@ public class MainActivity extends Activity implements Runnable, SensorEventListe
 //            }
 //        }
 
-    private void check (){
-        SimpleDateFormat sdf = new SimpleDateFormat();
-        sdf.applyPattern("yyyy/MM/dd HH:mm:ss");
-        Date d = null;
-        try {
-            d = sdf.parse("2016/10/09 13:00:00");
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        long timeStamp =  d.getTime() / 1000;
+//    private void check (){
+//
+//        recordNumber++;
+//        x = x+gx;
+//         y = y+gy;
+//         z = z + gz;
+//        int answer = rando.nextInt(4);
+//        if (recordNumber == 5) {
+//            recordNumber = 0;
+//
+//            if (answer == 0 && x/5 < -5) {
+//                hozon();
+//                make();
+//
+//
+//            } else if (answer == 1 && x/5 > 5) {
+//                hozon();
+//                make();
+//
+//            } else if (answer == 2 && y/5 < -5) {
+//                hozon();
+//                make();
+//            } else if (answer == 3 && y/5 > 5) {
+//                hozon();
+//                make();
+//
+//            } else {
+//                check();
+//            }
+//        }
+//
+//   }
 
-        int answer = rando.nextInt(4);
-
-        if (answer == 0 && gy < -3) {
-            number4++;
-            SharedPreferences data = getSharedPreferences("DataSave", Context.MODE_PRIVATE);
-            int Level = data.getInt("LevelSave",100);
-            int all = number4 + Level;
-            Log.d("kamui2",String.valueOf(data));
-            SharedPreferences.Editor editor = data.edit();
-            editor.putInt("LevelSave", all);
-            editor.putInt("Timer", (int) timeStamp);
-            editor.apply();
-
-            make();
-
-
-        }  else if (answer == 1 && gx > 5) {
-            number4++;
-            SharedPreferences data = getSharedPreferences("DataSave", Context.MODE_PRIVATE);
-            int Level = data.getInt("LevelSave",100);
-            int all = number4 + Level;
-            Log.d("kamui2",String.valueOf(data));
-            SharedPreferences.Editor editor = data.edit();
-            editor.putInt("LevelSave", all);
-            editor.apply();
-        make();
-
-        }  else if (answer == 2 && gx < -5) {
-            number4++;
-            SharedPreferences data = getSharedPreferences("DataSave", Context.MODE_PRIVATE);
-            int Level = data.getInt("LevelSave",100);
-            int all = number4 + Level;
-            Log.d("kamui2",String.valueOf(data));
-            SharedPreferences.Editor editor = data.edit();
-            editor.putInt("LevelSave", all);
-            editor.apply();
-        make();
-        }  else if (answer == 3 && gy > 5) {
-            number4++;
-            SharedPreferences data = getSharedPreferences("DataSave", Context.MODE_PRIVATE);
-            int Level = data.getInt("LevelSave",100);
-            int all = number4 + Level;
-            Log.d("kamui2",String.valueOf(data));
-            SharedPreferences.Editor editor = data.edit();
-            editor.putInt("LevelSave", all);
-            editor.apply();
-       make();
-
-        } else {
-        }
-
+    public void hozon() {
+        scoreNumber++;
+        SharedPreferences data = getSharedPreferences("DataSave", Context.MODE_PRIVATE);
+        int Level = data.getInt("LevelSave", 100);
+        int all = scoreNumber + Level;
+        Log.d("kamui2", String.valueOf(data));
+        SharedPreferences.Editor editor = data.edit();
+        editor.putInt("LevelSave", all);
+        editor.apply();
     }
-
-    
-
-
-
 
 
     @Override
     public void run() {
 
 
-
-        }
-
+    }
 
 
     protected void onResume() {
@@ -291,10 +313,10 @@ public class MainActivity extends Activity implements Runnable, SensorEventListe
         gx = event.values[0];
         gy = event.values[1];
         gz = event.values[2];
-        Log.d("acc", "X-axis : " + gx + "\n" +
-                "Y-axis : " + gy + "\n" +
-                "Z-axis : " + gz + "\n");
-        check();
+//        Log.d("acc", "X-axis : " + gx + "\n" +
+//                "Y-axis : " + gy + "\n" +
+//                "Z-axis : " + gz + "\n");
+        checkAnswer();
 
 
     }
