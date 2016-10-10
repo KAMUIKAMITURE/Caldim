348527e90c69175aea30ffc0f52bd7d596e0037b
KAMUIKAMITURE <tab.git.kamui@gmail.com>
2016-10-10 21:00:25 +0900

    new change

--- app/src/main/java/com/lifeistech/android/quiz/Main3Activity.java
+++ app/src/main/java/com/lifeistech/android/quiz/Main3Activity.java
@@ -2,15 +2,14 @@ package com.lifeistech.android.quiz;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.util.logging.Level;
-
 public class Main3Activity extends AppCompatActivity {
+    int nextLevel;
 
 
 
@@ -23,10 +22,20 @@ public class Main3Activity extends AppCompatActivity {
         Log.d("kamui",String.valueOf(data.getInt("LevelSave",100)));
         int Level = data.getInt("LevelSave",100);
         int level = data.getInt("Timer",100);
+        if (Level < 100) {
+            nextLevel = 100;
+        } else if (Level < 500) {
+            nextLevel = 500;
+        } else if (Level < 1000) {
+            nextLevel = 1000;
+        } else {
+            nextLevel = 0;
+        }
 
 
           String[] texts = {
-                "今まで正解した問題",String.valueOf(Level),"次のレベルアップまでに必要な正答数"
+                  "今まで正解した問題", String.valueOf(Level), "次のレベルアップまでに必要な正答数",
+                  String.valueOf(nextLevel)
 
         };
 
